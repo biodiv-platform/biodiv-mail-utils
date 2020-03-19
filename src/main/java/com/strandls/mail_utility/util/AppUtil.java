731fb9cb253cca0e9c715b98de2e4e379c7620eb
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-03-19 19:11:35 +0530

    :recycle: Added AppUtil

---
+++ src/main/java/com/strandls/mail_utility/util/AppUtil.java
@@ -0,0 +1,16 @@
+package com.strandls.mail_utility.util;
+
+import com.strandls.mail_utility.model.EnumModel.MAIL_TYPE;
+
+public class AppUtil {
+	
+	public static MAIL_TYPE getMailType(String type) {
+		for (MAIL_TYPE types: MAIL_TYPE.values()) {
+			if (types.name().equalsIgnoreCase(type)) {
+				return types;
+			}
+		}
+		return MAIL_TYPE.INVALID;
+	}
+
+}
