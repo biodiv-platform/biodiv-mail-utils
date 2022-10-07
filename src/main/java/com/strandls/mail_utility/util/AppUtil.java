a00bb3a8757d2e7d34a47a7dbde03f6fa452ad2d
vishnu prasath <teamdraftbox@gmail.com>
2022-10-07 17:58:03 +0530

    Tagged mail fix (#35)
    * chore: :robot: updated dev artifactory url
    
    * 🎨 tagged mail utility added
    
    Co-authored-by: Guddu Sharma <guddusharma027@gmail.com>
    Co-authored-by: Harsh Zalavadiya <harsh@strandls.com>

--- src/main/java/com/strandls/mail_utility/util/AppUtil.java
+++ src/main/java/com/strandls/mail_utility/util/AppUtil.java
@@ -1,6 +1,7 @@
 package com.strandls.mail_utility.util;
 
 import com.strandls.mail_utility.model.EnumModel.MAIL_TYPE;
+import com.strandls.mail_utility.model.EnumModel.OBJECT_TYPE;
 
 public final class AppUtil {
 	
@@ -13,6 +14,15 @@ public final class AppUtil {
 			}
 		}
 		return MAIL_TYPE.INVALID;
+	}
+	
+	public static OBJECT_TYPE  getObjectType(String type) {
+		for (OBJECT_TYPE types: OBJECT_TYPE.values()) {
+			if (types.name().equalsIgnoreCase(type)) {
+				return types;
+			}
+		}
+		return OBJECT_TYPE.INVALID;
 	}
 
 }
